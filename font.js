6d317af4ee9395a09542513e74303e6cedc07dc3
Erik Demaine <edemaine@mit.edu>
2021-07-24 15:24:36 -0400

    Initial font selection

---
+++ font.js
@@ -0,0 +1,98 @@
+window.font = {
+  "A": {
+    "puzzle": ".........\n.........\n...ooo...\n..o...o..\n..o.oo...\n..o......\n...o.o...\n..o.o.o..\n..o.o.o..\n....o....\noXo......",
+    "solution": "ooooooooo\noXXXXXXXo\noXoooooXo\noXoXXXoXo\noXoXoooXo\noXoXXXXXo\noXoooooXo\noXoXoXoXo\noXoXoXoXo\noXXXoXXXo\noXooooooo"
+  },
+  "B": {
+    "puzzle": ".........\n..X......\n...X.o...\n..X.o.o..\n.oo.o....\n.....ooo.\n.oo.o....\n....o.o..\n.oo..o...\n.........\noXo......",
+    "solution": "ooooooooo\noXXXXXXXo\noooXoooXo\noXXXoXoXo\noooXoXXXo\noXXXooooo\noooXoXXXo\noXXXoXoXo\noooXoooXo\noXXXXXXXo\noXooooooo"
+  },
+  "C": {
+    "puzzle": ".........\n..XXXXX..\n.X.....X.\n....X..X.\n..o...X..\n..o..ooo.\n..o.o....\n..o...o..\n...ooo...\n..o......\noX.o.....",
+    "solution": "ooooooooo\noXXXXXXXo\noXoooooXo\noXoXXXoXo\noXoXoXXXo\noXoXooooo\noXoXoXXXo\noXoXXXoXo\noXoooooXo\noXoXXXXXo\noXooooooo"
+  },
+  "D": {
+    "puzzle": ".........\n.........\n..X.oo...\n..X...o..\n..X.oo...\n......o..\n..X.oo...\n......o..\n.ooooo...\n..XX.....\no.X......",
+    "solution": "ooooooooo\noXXXXXXXo\nooXooooXo\noXXXXXoXo\nooXooooXo\noXXXXXoXo\nooXooooXo\noXXXXXoXo\noooooooXo\noXXXXXXXo\nooXoooooo"
+  },
+  "E": {
+    "puzzle": ".........\n.....XX..\n...o...X.\n..o..XX..\n..o.oooo.\n..o......\n..oX.ooo.\n..o......\n...ooo.X.\n..o......\noX.o.....",
+    "solution": "ooooooooo\noXXXXXXXo\noXoooooXo\noXoXXXXXo\noXoXooooo\noXoXXXXXo\noXoXooooo\noXoXXXXXo\noXoooooXo\noXoXXXXXo\noXooooooo"
+  },
+  "F": {
+    "puzzle": ".........\n.........\n...oooo..\n..o......\n..o.oooo.\n..o......\n...ooo.X.\n..o...X..\n..o.oX.X.\n..o.o....\noXo......",
+    "solution": "ooooooooo\noXXXXXXXo\noXoooooXo\noXoXXXXXo\noXoXooooo\noXoXXXXXo\noXoooooXo\noXoXXXXXo\noXoXoXoXo\noXoXoXoXo\noXooooooo"
+  },
+  "G": {
+    "puzzle": ".........\n.........\n...oooo..\n..o......\n..o..ooo.\n..o.o.X..\n..o.o....\n...o.o...\n.........\noX.o.....",
+    "solution": "ooooooooo\noXXXXXXXo\noXoooooXo\noXoXXXXXo\noXoXooooo\noXoXoXXXo\noXoXoXoXo\noXoooooXo\noXXXXXXXo\noXooooooo"
+  },
+  "H": {
+    "puzzle": ".........\n....o.X..\n..o.oX.X.\n..o.o....\n...o.o...\n..o...o..\n..o.o.o..\n..o.o.o..\n....o....\n.XX.o.X..\noX.......",
+    "solution": "ooooooooo\noXXXoXXXo\noXoXoXoXo\noXoXoXoXo\noXoooooXo\noXoXXXoXo\noXoXoXoXo\noXoXoXoXo\noXoXoXoXo\noXXXoXXXo\noXooooooo"
+  },
+  "I": {
+    "puzzle": ".........\n....X.X..\n.o.X.X...\n......X..\n.o.X.X...\n......X..\n.oo..X...\n..X.o.X..\n...XoX...\n.XX.o.X..\n...Xo....",
+    "solution": "ooooooooo\noXXXXXXXo\noooXoXooo\noXXXoXXXo\noooXoXooo\noXXXoXXXo\noooXoXooo\noXXXoXXXo\noooXoXooo\noXXXoXXXo\noooXooooo"
+  },
+  "J": {
+    "puzzle": ".........\n......X..\n..oo.X.X.\n....oX.X.\n.ooo.X.X.\n....o....\n..o.o.o..\n..o.o.o..\n...o..o..\n......o..\n......oXo",
+    "solution": "ooooooooo\noXXXXXXXo\noXoooXoXo\noXXXoXoXo\noooooXoXo\noXXXoXoXo\noXoXoXoXo\noXoXoXoXo\noXoooXoXo\noXXXXXoXo\noooooooXo"
+  },
+  "K": {
+    "puzzle": ".........\n..X.o....\n.X.Xo.o..\n.X.X.o...\n.X.......\n.X.X.ooo.\n.X..XXX..\n...X.o.X.\n..oX...X.\n..o...X..\noXo......",
+    "solution": "ooooooooo\noXXXoXXXo\noXoXoXoXo\noXoXoooXo\noXoXXXXXo\noXoXooooo\noXoXXXXXo\noXoXoooXo\noXoXoXoXo\noXoXoXXXo\noXooooooo"
+  },
+  "L": {
+    "puzzle": ".........\n..X.X....\n.X.X.Xo..\n...X.Xo..\n..o....o.\n..o.o....\n..o..ooo.\n..o......\n...ooooo.\n.........\noXo......",
+    "solution": "ooooooooo\noXXXXXXXo\noXoXoXoXo\noXoXoXoXo\noXoXoXooo\noXoXoXXXo\noXoXooooo\noXoXXXXXo\noXooooooo\noXXXXXXXo\noXooooooo"
+  },
+  "M": {
+    "puzzle": ".........\n..X.o.X..\n.X.Xo....\n.X.Xo.o..\n....o.o..\n..o.o.o..\n..o.o.o..\n..o...o..\n..oX.Xo..\n..o.X.o..\noX.o.....",
+    "solution": "ooooooooo\noXXXoXXXo\noXoXoXoXo\noXoXoXoXo\noXoXoXoXo\noXoXoXoXo\noXoXoXoXo\noXoXoXoXo\noXoXoXoXo\noXoXXXoXo\noXooooooo"
+  },
+  "N": {
+    "puzzle": ".........\n..X.o....\n.X.Xo.o..\n.X.X.....\n.X...o...\n.X..Xo...\n..X..Xo..\n.X...Xo..\n.X...Xo..\n..X......\noX.o.....",
+    "solution": "ooooooooo\noXXXoXXXo\noXoXoXoXo\noXoXoooXo\noXoXXoXXo\noXooXooXo\noXXoXXoXo\noXoooXoXo\noXoXoXoXo\noXXXoXXXo\noXooooooo"
+  },
+  "O": {
+    "puzzle": ".........\n.........\n...ooo...\n..o...o..\n..o.o.o..\n...o..o..\n......o..\n.ooooo...\n.........\noXo......",
+    "solution": "ooooooooo\noXXXXXXXo\noXoooooXo\noXoXXXoXo\noXoXoXoXo\noXoooXoXo\noXXXXXoXo\noooooooXo\noXXXXXXXo\noXooooooo"
+  },
+  "P": {
+    "puzzle": ".........\n.........\n.oo..o...\n....o.o..\n.oo.o.o..\n....o....\n.oo.oX.X.\n....o.X..\n.o.X.ooo.\n..X......\n...X.o...",
+    "solution": "ooooooooo\noXXXXXXXo\noooXoooXo\noXXXoXoXo\noooXoXoXo\noXXXoXoXo\noooXoXoXo\noXXXoXXXo\noooXooooo\noXXXXXXXo\noooXooooo"
+  },
+  "Q": {
+    "puzzle": ".........\n.........\n...ooo...\n..o.X.o..\n..o...o..\n..o.o.o..\n..o..o...\n..o......\n...ooo...\n......o..\n.....o.Xo",
+    "solution": "ooooooooo\noXXXXXXXo\noXoooooXo\noXoXXXoXo\noXoXoXoXo\noXoXoXoXo\noXoXoooXo\noXoXXXXXo\noXoooooXo\noXXXXXoXo\noooooooXo"
+  },
+  "R": {
+    "puzzle": ".........\n..X.XXX..\n...X.o.X.\n..X....X.\n.o.X..X..\n.....X.o.\n.o.X.....\n..X..Xo..\n...X..o..\n..X.o.o..\n...Xo....",
+    "solution": "ooooooooo\noXXXXXXXo\noooXoooXo\noXXXoXoXo\noooXoXXXo\noXXXoXooo\noooXoXXXo\noXXXoXoXo\noooXoXoXo\noXXXoXoXo\noooXooooo"
+  },
+  "S": {
+    "puzzle": ".........\n....XXX..\n.......X.\n..o.XXX..\n...ooooo.\n.........\n.ooooo...\n..XX..o..\n.X...o...\n..XX.....\noX.......",
+    "solution": "ooooooooo\noXXXXXXXo\noXoooooXo\noXoXXXXXo\noXooooooo\noXXXXXXXo\noooooooXo\noXXXXXoXo\noXoooooXo\noXXXXXXXo\noXooooooo"
+  },
+  "T": {
+    "puzzle": ".........\n..X.X.X..\n.X.X.X.X.\n...X.X.X.\n.o.X.X...\n......X..\n.o.X.X...\n......X..\n.o.X.X...\n......X..\n..oX.....",
+    "solution": "ooooooooo\noXXXXXXXo\noXoXoXoXo\noXoXoXoXo\noooXoXooo\noXXXoXXXo\noooXoXooo\noXXXoXXXo\noooXoXooo\noXXXoXXXo\noooXooooo"
+  },
+  "U": {
+    "puzzle": ".........\n..X.o....\n.X.X..o..\n.X.X.Xo..\n...X.Xo..\n..oX.Xo..\n..oX.Xo..\n..o.X.o..\n...ooo.o.\n.........\noXo......",
+    "solution": "ooooooooo\noXXXoXXXo\noXoXoXoXo\noXoXoXoXo\noXoXoXoXo\noXoXoXoXo\noXoXoXoXo\noXoXXXoXo\noXooooooo\noXXXXXXXo\noXooooooo"
+  },
+  "X": {
+    "puzzle": ".........\n..X.o....\n.X.Xo.o..\n.X...o...\n..X..X...\n.oo.X..o.\n....XXX..\n...o...X.\n..o.oX.X.\n....o.X..\noXo......",
+    "solution": "ooooooooo\noXXXoXXXo\noXoXoXoXo\noXoooooXo\noXXXXXXXo\nooooXoooo\noXXXXXXXo\noXoooooXo\noXoXoXoXo\noXXXoXXXo\noXooooooo"
+  },
+  "Y": {
+    "puzzle": ".........\n....o.X..\n..o.o....\n..o.o.o..\n..o...o..\n...o.o...\n....o....\n.o..o..o.\n..o.o.o..\n..X.o....\n...Xo....",
+    "solution": "ooooooooo\noXXXoXXXo\noXoXoXoXo\noXoXoXoXo\noXoXXXoXo\noXoooooXo\noXXXoXXXo\noooXoXooo\noXoXoXoXo\noXXXoXXXo\noooXooooo"
+  },
+  "Z": {
+    "puzzle": ".........\n.........\n..oooo...\n......o..\n.ooooo...\n.........\n...ooooo.\n..o......\n...oooo..\n.........\noXo......",
+    "solution": "ooooooooo\noXXXXXXXo\noXoooooXo\noXXXXXoXo\noooooooXo\noXXXXXXXo\noXooooooo\noXoXXXXXo\noXoooooXo\noXXXXXXXo\noXooooooo"
+  }
+};
